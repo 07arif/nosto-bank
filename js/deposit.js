f4a762d3e71ad9c200d0dee373e15a97d60544a9
07arif <ai.arif0711@gmail.com>
2022-09-24 16:11:37 +0600

    diposit problem solved

--- js/deposit.js
+++ js/deposit.js
@@ -7,20 +7,24 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     3. convert string value to a number
     */
   const newDepositAmount = getInputFieldValueById("deposit-field");
+  
   /* 
     1. get previous deposit total by id
 
     */
   const previousDepositTotal = getTextElementValueById("deposit-total");
-
+  const previousDepositString = parseFloat(previousDepositTotal);
   // calculate new deposit total
-  const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  const newDepositTotal = previousDepositString + newDepositAmount;
+ 
+  // console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
   // set deposit total value
   setTextElementValueById("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+  const previousBalanceTotalstring = parseFloat(previousBalanceTotal);
+  const newBalanceTotal = previousBalanceTotalstring + newDepositAmount;
+
   setTextElementValueById("balance-total", newBalanceTotal);
 });
